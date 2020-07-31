64356b132aa01524cd41209c733021a7552b1c3d
spamz23 <diogo_silva30@hotmail.com>
2020-07-31 02:29:26 +0100

    add log

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ def job_result(job_key):
         abort(404)  # unexistent job
 
     if job.is_finished:
+        print("job result:"+job.result)
         result = db.GetOne_By_Data(job.result)
         return jsonify({"status": "ok", "URI": url_for('get_news', _external=True), "data": json.loads(result)})
     else:
