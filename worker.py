1e7ab8aeb039adbf0e82a0ca00b57ad9a4748a01
spamz23 <diogo_silva30@hotmail.com>
2020-09-30 03:12:59 +0100

    fix localhost redis

--- worker.py
+++ worker.py
@@ -5,12 +5,11 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'unix://localhost:6379')
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
-        print("WORKING")
         worker = Worker(list(map(Queue, listen)))
         worker.work()
