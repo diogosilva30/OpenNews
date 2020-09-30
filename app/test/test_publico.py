e3347cdfe4e9614cab9594359adfbd24701fb422
spamz23 <diogo_silva30@hotmail.com>
2020-09-30 21:57:25 +0100

    add tests for publico search with invalid dates

--- app/test/test_publico.py
+++ app/test/test_publico.py
@@ -135,8 +135,8 @@ class TestPublicoKeywordsSearch(BaseTestCase):
         self.assert400(
             response, "Keyword search should trigger 'Bad Request'! Date range is too big")
 
-    def test_invalid_dates__keywords_search_job(self):
-        """ Test for Publico keywords search with invalid date range (greater than 3 months) """
+    def test_invalid_dates_keywords_search_job(self):
+        """ Test for Publico keywords search with invalid date format """
         response = self.client.post("/api/v1/news/publico/keywords_search",
                                     json={
                                         "start_date": "not a valid date",
@@ -150,6 +150,22 @@ class TestPublicoKeywordsSearch(BaseTestCase):
 
         self.assert400(
             response, "Keyword search should trigger 'Bad Request'! Dates are invalid")
+
+    def test_starting_date_older_than_ending_date(self):
+        """ Test for Publico keyword search with starting date > ending date """
+        response = self.client.post("/api/v1/news/publico/keywords_search",
+                                    json={
+                                        "start_date": "5/03/2020",
+                                        "end_date": "10/3/2019",
+                                        "keywords": "covid"
+                                    })
+
+        self.assertEqual(response.json["status"], "error")
+        self.assertIn("Invalid dates provided",
+                      response.json["message"])
+
+        self.assert400(
+            response, "Keyword search should trigger 'Bad Request'! Dates are invalid (starting date > ending date)")
 
 
 class TestPublicoTopicSearch(BaseTestCase):
@@ -167,6 +183,47 @@ class TestPublicoTopicSearch(BaseTestCase):
         response_json = get_results_from_fake_queue(search_job.id).json
 
         self.assertIn("number of found news", response_json)
+
+    def test_topic_search_job(self):
+        """ Test for Publico topic fake job creation and news retrieval"""
+        data = {
+            "start_date": "1/3/2020",
+            "end_date": "15/3/2020",
+            "search_topic": "luanda leaks"
+        }
+        self._enqueue_topic_search_job(data)
+
+    def test_invalid_dates_topic_search_job(self):
+        """ Test for Publico topic search with invalid date format """
+        response = self.client.post("/api/v1/news/publico/topic_search",
+                                    json={
+                                        "start_date": "not a valid date",
+                                        "end_date": "another invalid date",
+                                        "keywords": "covid"
+                                    })
+
+        self.assertEqual(response.json["status"], "error")
+        self.assertIn("Invalid date string format provided",
+                      response.json["message"])
+
+        self.assert400(
+            response, "Keyword search should trigger 'Bad Request'! Dates are invalid")
+
+    def test_starting_date_older_than_ending_date(self):
+        """ Test for Publico topic search with starting date > ending date """
+        response = self.client.post("/api/v1/news/publico/topic_search",
+                                    json={
+                                        "start_date": "5/03/2020",
+                                        "end_date": "10/3/2019",
+                                        "keywords": "covid"
+                                    })
+
+        self.assertEqual(response.json["status"], "error")
+        self.assertIn("Invalid dates provided",
+                      response.json["message"])
+
+        self.assert400(
+            response, "Keyword search should trigger 'Bad Request'! Dates are invalid (starting date > ending date)")
 
 
 if __name__ == '__main__':
