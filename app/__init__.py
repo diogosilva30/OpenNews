9c8cf23f8ba7a43290075d7ca849f0ad9b4fac2f
spamz23 <diogo_silva30@hotmail.com>
2020-10-01 01:45:20 +0100

    :green_heart: FIX FOR LATEST BUILD

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,4 @@
-# from .apis.v1 import api as api_v1
+# Fix for flask-testing bug. See: https://github.com/jarus/flask-testing/issues/143
+import werkzeug
 
-# # # Fix for flask-testing bug. See: https://github.com/jarus/flask-testing/issues/143
-# # import werkzeug
-
-# # werkzeug.cached_property = werkzeug.utils.cached_property
+werkzeug.cached_property = werkzeug.utils.cached_property
