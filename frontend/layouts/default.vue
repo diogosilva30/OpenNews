e41c5bd82492a1475c0698d15799d601b0f9b1d4
spamz23 <diogo_silva30@hotmail.com>
2021-02-10 20:00:31 +0000

    :sparkles: Add Initial Frontend

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-main>
+      <nuxt-child />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container fluid fill-height class="blue">
+    <v-row>
+      <v-card tile flat class="white" width="70%" height="100vh"></v-card>
+      <v-card id="test" tile flat class="white" width="20%" height="100vh">
+      </v-card>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
+<style scoped>
+#test {
+  -webkit-transform: skewX(-5deg);
+  -moz-transform: skewX(-5deg);
+  -ms-transform: skewX(-5deg);
+  -o-transform: skewX(-5deg);
+  transform: skewX(-5deg);
+}
+</style>
